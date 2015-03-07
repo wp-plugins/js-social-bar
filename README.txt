2b012da41225b16476eda794458078af66ce1b7e
JediSoftware <JediSoftware@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-07 09:45:08 +0000

    Trying to fix wordpress plugin name in the Plugin Directory
    git-svn-id: https://plugins.svn.wordpress.org/js-social-bar/trunk@1107513 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.txt
+++ README.txt
@@ -4,7 +4,6 @@ Donate link: http://www.jedi-software.com/donate
 Tags: social, sidebar, google, twitter, facebook, widget, plugin
 Requires at least: 3.3
 Tested up to: 4.1.1
-Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- js_social_bar.php
+++ js_social_bar.php
@@ -1,11 +1,31 @@
-<?phpdefined('ABSPATH') or die;include "includes/social_html_codes.php";
+<?php
+defined('ABSPATH') or die;
+include "includes/social_html_codes.php";
+
 /*
-Plugin Name: JS Social BarAuthor URI: http://www.jedi-software.com/wordpress/js-social-bar
+
+Plugin Name: JS Social Bar
+Author URI: http://www.jedi-software.com/wordpress/js-social-bar
+
 Description: A fancy sidebar to link all your social network pages in your Wordpress site!
+
 Version: 1.0
+
 Author: Jedi Software
+
 Author URI: http://www.jedi-software.com
+
 License: GPL
-*/function a() {}register_uninstall_hook('uninstall.php', a);/* SITE - FRONTEND */include "site_functions.php";/* ADMINISTRATION */
+
+*/
+
+function a() {}
+
+register_uninstall_hook('uninstall.php', a);
+/* SITE - FRONTEND */
+include "site_functions.php";
+
+/* ADMINISTRATION */
+
 include "admin_functions.php";
 ?>
